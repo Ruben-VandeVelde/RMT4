b3c1250397ce55cb8d290bc434e3ef8e8ad84d78
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-04-01 13:53:22 +0200

    x

--- RMT4/Spaces.lean
+++ RMT4/Spaces.lean
@@ -85,7 +85,7 @@ lemma 𝓘_nonempty [good_domain U] : (𝓘 U).Nonempty := by
     have e2 := g_hol.analyticAt e1
     have f_eq_comp := (good_domain.is_open.eventually_mem hz₀).mono g_sqf
     have dg_nonzero : deriv g z₀ ≠ 0 := by
-      have e3 := e2.differentiableAt.deriv_eq_deriv_pow_div_pow zero_lt_two f_eq_comp (f_noz hz₀)
+      have e3 := e2.differentiableAt.deriv_eq_deriv_pow_div_pow two_ne_zero f_eq_comp (f_noz hz₀)
       simp [e3, f]
       intro h
       have := g_sqf hz₀
--- RMT4/to_mathlib.lean
+++ RMT4/to_mathlib.lean
@@ -1,44 +1,48 @@
+import Mathlib.Analysis.Analytic.IsolatedZeros
 import Mathlib.Analysis.Calculus.ParametricIntegral
+import Mathlib.Analysis.Complex.RemovableSingularity
+import Mathlib.MeasureTheory.Integral.CircleIntegral
 import Mathlib.MeasureTheory.Integral.IntervalIntegral.IntegrationByParts
-import RMT4.cindex
 
 open intervalIntegral Real MeasureTheory Filter Topology Set Metric Interval
 
 variable {𝕜 E V : Type*} {r : ℝ} {z : ℂ} {a b t : ℝ} {n : ℕ}
 
-lemma isCompact_segment [Ring 𝕜] [PartialOrder 𝕜] [IsOrderedRing 𝕜] [TopologicalSpace 𝕜]
-    [IsTopologicalAddGroup 𝕜] [CompactIccSpace 𝕜] [TopologicalSpace E] [AddCommGroup E]
-    [ContinuousAdd E] [Module 𝕜 E] [ContinuousSMul 𝕜 E] {x y : E} :
+-- Mathlib/Analysis/Convex/Topology.lean
+lemma isCompact_segment {𝕜 E : Type*}
+    [Ring 𝕜] [PartialOrder 𝕜] [IsOrderedRing 𝕜] [TopologicalSpace 𝕜] [ContinuousSub 𝕜]
+    [CompactIccSpace 𝕜] [TopologicalSpace E] [AddCommGroup E] [ContinuousAdd E] [Module 𝕜 E]
+    [ContinuousSMul 𝕜 E] {x y : E} :
     IsCompact (segment 𝕜 x y) := by
-  simpa only [segment_eq_image] using isCompact_Icc.image (by continuity)
+  simpa only [segment_eq_image] using isCompact_Icc.image (by fun_prop)
 
-lemma DifferentiableAt.deriv_eq_deriv_pow_div_pow {n : ℕ} (n_pos : 0 < n) {f g : ℂ → ℂ}
-    (hg : ∀ᶠ z in 𝓝 z, f z = (g z) ^ n) (g_diff : DifferentiableAt ℂ g z) (fz_nonzero : f z ≠ 0) :
-    deriv g z = deriv f z / (n * (g z) ^ (n - 1)) := by
-  have h1 : g z ≠ 0 := λ h => fz_nonzero (by simp [Eventually.self_of_nhds hg, h, n_pos.ne.symm])
-  have h2 : n * (g z) ^ (n - 1) ≠ 0 := by simp [h1, n_pos.ne.symm]
-  rw [(EventuallyEq.deriv hg).self_of_nhds, deriv_fun_pow g_diff, eq_div_iff h2]
+lemma DifferentiableAt.deriv_eq_deriv_pow_div_pow {n : ℕ} (hn : n ≠ 0) {f g : ℂ → ℂ}
+    (hg : f =ᶠ[𝓝 z] (g · ^ n)) (g_diff : DifferentiableAt ℂ g z) (fz_nonzero : f z ≠ 0) :
+    deriv g z = deriv f z / (n * g z ^ (n - 1)) := by
+  have h1 : g z ≠ 0 := fun h => fz_nonzero (by simp [hg.self_of_nhds, h, hn])
+  have h2 : n * (g z) ^ (n - 1) ≠ 0 := by simp [h1, hn]
+  rw [hg.deriv.self_of_nhds, deriv_fun_pow g_diff, eq_div_iff h2]
   ring
 
 lemma has_deriv_at_integral_of_continuous_of_lip
     {φ : ℂ → ℝ → ℂ} {ψ : ℝ → ℂ} {z₀ : ℂ} {a b C δ : ℝ} (hab : a ≤ b) (δ_pos : 0 < δ)
     (φ_cts : ∀ᶠ z in 𝓝 z₀, ContinuousOn (φ z) (Icc a b))
-    (φ_der : ∀ t ∈ Ioc a b, HasDerivAt (λ x => φ x t) (ψ t) z₀)
-    (φ_lip : ∀ t ∈ Ioc a b, LipschitzOnWith (Real.nnabs C) (λ x => φ x t) (ball z₀ δ))
+    (φ_der : ∀ t ∈ Ioc a b, HasDerivAt (fun x => φ x t) (ψ t) z₀)
+    (φ_lip : ∀ t ∈ Ioc a b, LipschitzOnWith (Real.nnabs C) (fun x => φ x t) (ball z₀ δ))
     (ψ_cts : ContinuousOn ψ (Ioc a b)) :
-    HasDerivAt (λ z => ∫ t in a..b, φ z t) (∫ t in a..b, ψ t) z₀ := by
+    HasDerivAt (fun z => ∫ t in a..b, φ z t) (∫ t in a..b, ψ t) z₀ := by
   simp only [intervalIntegral, not_lt, hab, Ioc_eq_empty, Measure.restrict_empty,
     integral_zero_measure, sub_zero]
   let μ : Measure ℝ := volume.restrict (Ioc a b)
   have h1 : ∀ᶠ z in 𝓝 z₀, AEStronglyMeasurable (φ z) μ :=
-    φ_cts.mono (λ z h => (h.mono Ioc_subset_Icc_self).aestronglyMeasurable measurableSet_Ioc)
+    φ_cts.mono (fun z h => (h.mono Ioc_subset_Icc_self).aestronglyMeasurable measurableSet_Ioc)
   have h2 : Integrable (φ z₀) μ :=
     φ_cts.self_of_nhds.integrableOn_Icc.mono_set Ioc_subset_Icc_self
   have h3 : AEStronglyMeasurable ψ μ := ψ_cts.aestronglyMeasurable measurableSet_Ioc
-  have h4 : ∀ᵐ t ∂μ, LipschitzOnWith (Real.nnabs C) (λ z => φ z t) (ball z₀ δ) :=
+  have h4 : ∀ᵐ t ∂μ, LipschitzOnWith (Real.nnabs C) (fun z => φ z t) (ball z₀ δ) :=
     (ae_restrict_iff' measurableSet_Ioc).mpr (Eventually.of_forall φ_lip)
-  have h5 : Integrable (λ _ => C) μ := integrable_const _
-  have h6 : ∀ᵐ t ∂μ, HasDerivAt (λ z => φ z t) (ψ t) z₀ :=
+  have h5 : Integrable (fun _ => C) μ := integrable_const _
+  have h6 : ∀ᵐ t ∂μ, HasDerivAt (fun z => φ z t) (ψ t) z₀ :=
     (ae_restrict_iff' measurableSet_Ioc).mpr (Eventually.of_forall φ_der)
   exact (hasDerivAt_integral_of_dominated_loc_of_lip δ_pos h1 h2 h3 h4 h5 h6).2
 
@@ -76,7 +80,7 @@ lemma eventually_mem_uIoo_of_mem_uIoc : ∀ᵐ x, x ∈ Ι a b → x ∈ uIoo a 
   apply eventually_of_mem (U := {a, b}ᶜ)
   · simpa only [mem_ae_iff, compl_compl] using measure_union_null volume_singleton volume_singleton
   · rw [uIoo_eq_uIoc_sdiff_ends]
-    exact λ t h1 h2 => ⟨h2, h1⟩
+    exact fun t h1 h2 => ⟨h2, h1⟩
 end uIoo
 
 section helper_integral
@@ -123,7 +127,7 @@ omit [CompleteSpace E] in
 theorem integral_derivWithin_smul_comp
     (hg : ContDiffOn ℝ 1 g (uIcc a b)) (hf : ContinuousOn f (g '' uIcc a b)) :
     (∫ x in a..b, derivWithin g (uIcc a b) x • (f ∘ g) x) = (∫ x in g a..g b, f x) := by
-  refine integral_comp_smul_deriv'' hg.continuousOn (λ t ht => ?_) (hg.continuousOn_derivWithin'' le_rfl) hf
+  refine integral_comp_smul_deriv'' hg.continuousOn (fun t ht => ?_) (hg.continuousOn_derivWithin'' le_rfl) hf
   apply (hg.differentiableOn le_rfl t (Set.uIoo_subset_uIcc_self ht)).hasDerivWithinAt.mono_of_mem_nhdsWithin
   exact uIcc_mem_nhds_within ht
 
